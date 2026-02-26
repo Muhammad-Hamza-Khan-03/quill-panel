03f98871adaa5af9ae9e504a4b2116df506a43ca
hamzakhan-03 <hamzakhan102003@gmail.com>
2026-02-26 06:39:13 +0500

    fixed: added trailing

--- src/app/admin/inventory/edit/[id]/page.tsx
+++ src/app/admin/inventory/edit/[id]/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 export const dynamic = 'force-dynamic';
+export const dynamicParams = true;
 export const runtime = 'edge';
 
 import React, { useState, useEffect, useRef } from 'react';
